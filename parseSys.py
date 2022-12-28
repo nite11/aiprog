eae3b9a1f96ff4958339c5e9961bba05c67ec8d2
nitesh <nitesh.passau@gmail.com>
2022-12-28 07:36:26 +0100

    1

---
+++ parseSys.py
@@ -0,0 +1,23 @@
+with open('sys.txt','r') as f:
+    contentL=f.read().lower()##.splitlines()    
+    f.seek(0)
+    content=f.read()
+
+equations=""
+constraints=""
+
+if contentL.find("such")!=-1:
+    equations=content[contentL.find("solve")+5:contentL.find("such")]
+    constraints=content[contentL.find("that")+4:contentL.find(".")]
+else:
+    equations=content[contentL.find("solve")+5:contentL.find(".")]
+    
+
+equations=equations.strip().replace("\n", "").replace("=", "==")
+constraints=constraints.strip().replace("\n", "").replace(" ", "")
+print(equations)
+print(constraints)
+
+
+
+
